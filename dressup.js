4e062d8908ce118d27c5a4a4d3f4a84b57df6699
Laura Chow <l.chow.tech@gmail.com>
2025-01-06 15:44:21 -0800

    added ability to drag hats on canvas

--- dressup.js
+++ dressup.js
@@ -1,14 +1,97 @@
 // declare canvas/board related variables
 var canvasWidth = 600;
 var canvasHeight = 600;
+var hatImages = [];
+var context;
+var canvas;
+var baseFigure;
+var isDragging = false;
+var imageIntersect;
 window.onload = function () {
-    var canvas = document.getElementById("canvas");
+    canvas = document.getElementById("canvas");
     canvas.height = canvasHeight;
     canvas.width = canvasWidth;
-    var context = canvas.getContext("2d");
-    var baseFigure = new Image();
+    context = canvas.getContext("2d");
+    baseFigure = new Image();
     baseFigure.src = "base_figure.png";
     baseFigure.addEventListener("load", function () {
         context === null || context === void 0 ? void 0 : context.drawImage(baseFigure, 200, 100);
     });
+    // TODO: draw hats
+    // TODO: draw outfits
+    var hatArray = ["hat_orange.png", "hat_green.png", "hat_propeller.png"];
+    var _loop_1 = function (i) {
+        var image = new Image();
+        image.setAttribute("id", "hat" + i);
+        // hatImage.addEventListener("load", () => {
+        //   context?.drawImage(hatImage, 75, 75 + 100 * i);
+        // });
+        image.src = hatArray[i];
+        var hatImage = {
+            image: image,
+            x: 75,
+            y: 75 + 100 * i,
+            width: image.width,
+            height: image.height,
+        };
+        hatImages.push(hatImage);
+        // make hats draggable
+        hatImage.image.onmousedown = function (e) {
+            hatImage.x = e.clientX;
+            hatImage.y = e.clientY;
+            animate();
+        };
+        hatImage.image.onmouseup = function (e) { };
+    };
+    for (var i = 0; i < hatArray.length; i++) {
+        _loop_1(i);
+    }
+    canvas.onmousedown = function (e) {
+        console.log("in canvas mousedown");
+        var rect = canvas.getBoundingClientRect();
+        for (var i = hatImages.length - 1; i >= 0; i--) {
+            if (testImageIntersect(hatImages[i], e.clientX - rect.left, e.clientY - rect.top)) {
+                imageIntersect = hatImages[i];
+                break;
+            }
+        }
+        if (imageIntersect) {
+            isDragging = true;
+        }
+    };
+    canvas.onmousemove = function (e) {
+        var rect = canvas.getBoundingClientRect();
+        if (isDragging) {
+            console.log(imageIntersect);
+            imageIntersect.x += e.movementX;
+            imageIntersect.y += e.movementY;
+        }
+    };
+    canvas.onmouseup = function (e) {
+        isDragging = false;
+        imageIntersect = null;
+    };
+    animate();
+};
+var animate = function () {
+    requestAnimationFrame(animate);
+    context.clearRect(0, 0, canvasWidth, canvasHeight);
+    context.drawImage(baseFigure, 200, 100);
+    for (var i = 0; i < hatImages.length; i++) {
+        var hatImage = hatImages[i];
+        context === null || context === void 0 ? void 0 : context.drawImage(hatImage.image, hatImage.x, hatImage.y);
+    }
+};
+// x = mouse x position
+// y = mouse y position
+var testImageIntersect = function (elem, x, y) {
+    // if x is in range of width of image
+    // and y is in range of height of image
+    if (x <= elem.x + elem.width &&
+        x >= elem.x &&
+        y <= elem.y + elem.height &&
+        y >= elem.y) {
+        return true;
+    }
+    return false;
 };
--- dressup.ts
+++ dressup.ts
@@ -1,20 +1,129 @@
 // declare canvas/board related variables
 let canvasWidth = 600;
 let canvasHeight = 600;
+let hatImages: DraggableImage[] = [];
+let context: CanvasRenderingContext2D;
+let canvas: HTMLCanvasElement;
+let baseFigure: HTMLImageElement;
+
+let isDragging = false;
+let imageIntersect: DraggableImage;
+
+type DraggableImage = {
+  image: HTMLImageElement;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
 
 window.onload = () => {
-  let canvas = <HTMLCanvasElement>document.getElementById("canvas");
+  canvas = <HTMLCanvasElement>document.getElementById("canvas");
   canvas.height = canvasHeight;
   canvas.width = canvasWidth;
 
-  let context = canvas.getContext("2d");
+  context = canvas.getContext("2d");
 
-  let baseFigure = new Image();
+  baseFigure = new Image();
   baseFigure.src = "base_figure.png";
   baseFigure.addEventListener("load", () => {
     context?.drawImage(baseFigure, 200, 100);
   });
 
   // TODO: draw hats
-  // TODO: draw
+  // TODO: draw outfits
+  let hatArray = ["hat_orange.png", "hat_green.png", "hat_propeller.png"];
+  for (let i = 0; i < hatArray.length; i++) {
+    let image = new Image();
+
+    image.setAttribute("id", "hat" + i);
+    // hatImage.addEventListener("load", () => {
+    //   context?.drawImage(hatImage, 75, 75 + 100 * i);
+    // });
+
+    image.src = hatArray[i];
+    let hatImage: DraggableImage = {
+      image: image,
+      x: 75,
+      y: 75 + 100 * i,
+      width: image.width,
+      height: image.height,
+    };
+
+    hatImages.push(hatImage);
+    // make hats draggable
+    hatImage.image.onmousedown = (e: MouseEvent) => {
+      hatImage.x = e.clientX;
+      hatImage.y = e.clientY;
+      animate();
+    };
+
+    hatImage.image.onmouseup = (e: MouseEvent) => {};
+  }
+
+  canvas.onmousedown = (e: MouseEvent) => {
+    let rect = canvas.getBoundingClientRect();
+    for (let i = hatImages.length - 1; i >= 0; i--) {
+      if (
+        testImageIntersect(
+          hatImages[i],
+          e.clientX - rect.left,
+          e.clientY - rect.top
+        )
+      ) {
+        imageIntersect = hatImages[i];
+        break;
+      }
+    }
+    if (imageIntersect) {
+      isDragging = true;
+    }
+  };
+
+  canvas.onmousemove = (e: MouseEvent) => {
+    let rect = canvas.getBoundingClientRect();
+    if (isDragging) {
+      console.log(imageIntersect);
+      imageIntersect.x += e.movementX;
+      imageIntersect.y += e.movementY;
+    }
+  };
+
+  canvas.onmouseup = (e: MouseEvent) => {
+    isDragging = false;
+    imageIntersect = null;
+  };
+
+  animate();
+};
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  context.clearRect(0, 0, canvasWidth, canvasHeight);
+  context.drawImage(baseFigure, 200, 100);
+
+  for (let i = 0; i < hatImages.length; i++) {
+    let hatImage = hatImages[i];
+    context?.drawImage(hatImage.image, hatImage.x, hatImage.y);
+  }
+};
+
+// x = mouse x position
+// y = mouse y position
+const testImageIntersect = (
+  elem: DraggableImage,
+  x: number,
+  y: number
+): boolean => {
+  // if x is in range of width of image
+  // and y is in range of height of image
+  if (
+    x <= elem.x + elem.width &&
+    x >= elem.x &&
+    y <= elem.y + elem.height &&
+    y >= elem.y
+  ) {
+    return true;
+  }
+  return false;
 };
